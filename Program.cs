01c63d09ef3a04c4b97769225cec2141e7c8f014
Iwantmorebugs <zakariawakrim@gmail.com>
2021-07-21 18:53:06 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client1 = DatabaseClient.GetInstance;
+            var client2 = DatabaseClient.GetInstance;
+
+            client1.WriteRecord();
+            client2.ReadRecord();
+
+            Console.WriteLine(client2==client1);
+            Console.Read();
+        }
+    }
+
+    public class DatabaseClient
+    {
+        private DatabaseClient()
+        {
+            Console.WriteLine("Connection Established with the db.");
+        }
+
+        public static DatabaseClient _instance = new DatabaseClient();
+        public static DatabaseClient GetInstance => _instance;
+
+        public void WriteRecord()
+        {
+            Console.WriteLine("Wrote record to the db.");
+        }
+
+        public void ReadRecord()
+        {
+            Console.WriteLine("Read record from the db.");
+        }
+    }
+}
